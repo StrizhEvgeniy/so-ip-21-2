82f0a9facadd85828729dd3acb27e8202a7e0973
Evgen <strizh.evgeniy@gmail.com>
2024-01-20 11:08:21 +1000

    sgsyts

--- first_app/src/components/good/Good.jsx
+++ first_app/src/components/good/Good.jsx
@@ -6,7 +6,9 @@ export default function Good() {
   return (
     <div className={styles.good}>
       <h2 className={styles.title}>Монитор</h2>
-      <img className={styles.image} src="monitor.jpg" alt="" />
+      <div className={styles.imageContainer}>
+        <img className={styles.image} src="monitor.jpg" alt="" />
+      </div>
       <div className={styles.footer}>
         <div className={styles.price}>5795</div>
         <div className={styles.stock}>В наличии 1</div>
